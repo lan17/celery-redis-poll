484ec59256e7e31b4b593ad4c650b131bab8e50a
Lev Neiman <lev.neiman@gmail.com>
2025-01-23 13:52:35 -0800

    remove install from prod code

--- src/celery_redis_poll_backend/__init__.py
+++ src/celery_redis_poll_backend/__init__.py
@@ -6,12 +6,4 @@ from celery_redis_poll_backend.backend import (
 )
 
 
-def install_redis_poll_backend():
-    """Explicitly install.  Useful for local unit tests."""
-    from celery.app.backends import BACKEND_ALIASES
-
-    BACKEND_ALIASES["redis"] = "celery_redis_poll.backend:choose_redis_backend"
-    BACKEND_ALIASES["rediss"] = "celery_redis_poll.backend:choose_rediss_backend"
-
-
 __all__ = ["PollingRedisBackend", "PollingRedisClusterBackend"]
--- tests/test_redis_poll_backend.py
+++ tests/test_redis_poll_backend.py
@@ -9,8 +9,15 @@ import pytest
 from celery_redis_poll_backend import (
     PollingRedisBackend,
     PollingRedisClusterBackend,
-    install_redis_poll_backend,
 )
+
+
+def install_redis_poll_backend():
+    """Explicitly install.  Useful for local unit tests."""
+    from celery.app.backends import BACKEND_ALIASES
+
+    BACKEND_ALIASES["redis"] = "celery_redis_poll.backend:choose_redis_backend"
+    BACKEND_ALIASES["rediss"] = "celery_redis_poll.backend:choose_rediss_backend"
 
 
 @mark.parametrize(
