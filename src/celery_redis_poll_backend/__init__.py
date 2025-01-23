e1fce466d1a98b8ec0699420c30986aa7065876a
Lev Neiman <lev.neiman@gmail.com>
2025-01-23 13:51:51 -0800

    update install command

--- src/celery_redis_poll_backend/__init__.py
+++ src/celery_redis_poll_backend/__init__.py
@@ -10,10 +10,8 @@ def install_redis_poll_backend():
     """Explicitly install.  Useful for local unit tests."""
     from celery.app.backends import BACKEND_ALIASES
 
-    BACKEND_ALIASES["redispoll"] = "celery_redis_poll.backend:PollingRedisBackend"
-    BACKEND_ALIASES["redisclusterpoll"] = (
-        "celery_redis_poll.backend:PollingRedisClusterBackend"
-    )
+    BACKEND_ALIASES["redis"] = "celery_redis_poll.backend:choose_redis_backend"
+    BACKEND_ALIASES["rediss"] = "celery_redis_poll.backend:choose_rediss_backend"
 
 
 __all__ = ["PollingRedisBackend", "PollingRedisClusterBackend"]
